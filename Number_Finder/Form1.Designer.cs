5034be258697f7564f0dd7a5af16e196753ca748
stagiaire <hungreman400@gmail.com>
2025-06-27 11:35:00 +0200

    Ajoutez des fichiers projet.

---
+++ Number_Finder/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Number_Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtField = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblTask = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nmb_of_tries = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblRnd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtField
+            // 
+            this.txtField.Location = new System.Drawing.Point(336, 153);
+            this.txtField.Name = "txtField";
+            this.txtField.Size = new System.Drawing.Size(100, 20);
+            this.txtField.TabIndex = 0;
+            this.txtField.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtField_KeyUp);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(349, 179);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblTask
+            // 
+            this.lblTask.AutoSize = true;
+            this.lblTask.Location = new System.Drawing.Point(292, 137);
+            this.lblTask.Name = "lblTask";
+            this.lblTask.Size = new System.Drawing.Size(189, 13);
+            this.lblTask.TabIndex = 2;
+            this.lblTask.Text = "Enter random number (1  -   1.000.000)";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.Control;
+            this.btnStart.Location = new System.Drawing.Point(311, 53);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(150, 25);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nmb_of_tries
+            // 
+            this.nmb_of_tries.AutoSize = true;
+            this.nmb_of_tries.Location = new System.Drawing.Point(446, 189);
+            this.nmb_of_tries.Name = "nmb_of_tries";
+            this.nmb_of_tries.Size = new System.Drawing.Size(35, 13);
+            this.nmb_of_tries.TabIndex = 4;
+            this.nmb_of_tries.Text = "label2";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(376, 91);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(13, 13);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "--";
+            // 
+            // lblRnd
+            // 
+            this.lblRnd.AutoSize = true;
+            this.lblRnd.Location = new System.Drawing.Point(704, 53);
+            this.lblRnd.Name = "lblRnd";
+            this.lblRnd.Size = new System.Drawing.Size(13, 13);
+            this.lblRnd.TabIndex = 6;
+            this.lblRnd.Text = "--";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRnd);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.nmb_of_tries);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTask);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtField;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblTask;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label nmb_of_tries;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblRnd;
+    }
+}
+
---
+++ Number_Finder/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Number_Finder
+{
+    public partial class Form1 : Form
+    {
+        private int myrndNumb = 0;
+        int amount_of_tries = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //generation our random number with button
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Random rng = new Random();
+            myrndNumb = rng.Next(1, 1000000);
+
+            lblRnd.Text = myrndNumb.ToString();
+            amount_of_tries = 0;
+
+            btnCheck.Visible = true;
+            txtField.Clear();
+            //txtField.Clear();
+
+            //if (txtField.Text.Length > 0);
+            //Application.Restart();
+
+        }
+
+
+        //cheking the height of number depending of our entered + filter of value
+        public void btnCheck_Click(object sender, EventArgs e)
+        {
+            check();
+
+        }
+
+
+
+        private void check()
+        {
+
+            if (string.IsNullOrEmpty(txtField.Text))
+            {
+                lblStatus.Text = "Write some thing before pressing button";
+            }
+
+            int entered_Numb = 0;
+            amount_of_tries = amount_of_tries + 1;
+            nmb_of_tries.Text = amount_of_tries.ToString();
+
+            if (int.TryParse(txtField.Text, out entered_Numb))
+            {
+                if (myrndNumb > entered_Numb)
+                    lblStatus.Text = "inferieur";
+                else if (myrndNumb < entered_Numb)
+                    lblStatus.Text = "superieur";
+                else if (myrndNumb == entered_Numb)
+                {
+                    lblStatus.Text = "trouver";
+                    btnCheck.Visible = false;
+                }
+            }
+            else if (string.IsNullOrEmpty(txtField.Text))
+            {
+                lblStatus.Text = "Write some thing before pressing button";
+            }
+            else
+            {
+                lblStatus.Text = "Wrong Forrmat";
+                Refresh();
+            }
+        }
+
+        private void txtField_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                check();
+            }
+        }
+    }
+}
+
+
